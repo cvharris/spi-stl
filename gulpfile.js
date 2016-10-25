98644dbd81c124ba76a1086f8c04f1b67327a814
harrisch <charlie.harris@wwt.com>
2016-10-25 10:27:35 -0500

    Moves favicons to img folder, adds addresses and other SEO stuff

--- gulpfile.js
+++ gulpfile.js
@@ -23,11 +23,8 @@ const nunjucks = require('gulp-nunjucks-render');
 const data = require('gulp-data');
 const fs = require('fs');
 const fontmin = require('gulp-fontmin');
-// const Fontmin = require('fontmin');
-// const fontmin = new Fontmin()
-//                 .src(`${source}${assets}fonts/CornerStoreJF.ttf`)
-//                 .use(Fontmin.ttf2woff())
-//                 .dest(`${source}${assets}fonts/built`);
+const gutil = require('gulp-util');
+const plumber = require('gulp-plumber');
 
 const processors = [
   normalize,
@@ -54,7 +51,12 @@ gulp.task('nunjucks', () => {
 gulp.task('styles', () => {
   return gulp.src(`${source}${assets}styles/main.less`)
     .pipe(sourcemaps.init())
-    .pipe(less())
+    .pipe(plumber())
+    .pipe(less().on('error', err => {
+        gutil.log(err);
+        this.emit('end');
+      })
+    )
     .pipe(postcss(processors))
     .pipe(cleancss(cleanCssOpts))
     .pipe(rename('main.min.css'))
@@ -92,6 +94,12 @@ gulp.task('scripts', () => {
     .pipe(browserSync.reload({stream:true}));
 });
 
+// copy static files
+gulp.task('static', () => {
+  return gulp.src(`*.+(xml|txt)`)
+    .pipe(gulp.dest(`${dest}`));
+});
+
 // start BrowserSync
 gulp.task('browser-sync', () => {
   browserSync.init({
@@ -110,15 +118,17 @@ gulp.task('clean', () => {
 // build files and watch for changes
 gulp.task('watch', ['clean'], () => {
   gulp.start('nunjucks', 'styles', 'scripts', 'images');
-  gulp.watch(`${source}templates/**/**/*.+(html|njk)`, ['nunjucks']);
-  gulp.watch(`${source}${assets}styles/*`, ['styles']);
-  gulp.watch(`${source}${assets}js/*`, ['scripts']);
-  gulp.watch(`${source}${assets}img/*`, ['images']);
+  gulp.watch([`${source}templates/**/**/*.+(html|njk)`, `${source}templates/**/*.+(html|njk)`, `${source}templates/*.+(html|njk)`], ['nunjucks']);
+  gulp.watch([`${source}${assets}styles/*.*`, `${source}${assets}styles/shared/*.less`], ['styles']);
+  gulp.watch(`${source}${assets}js/*.*`, ['scripts']);
+  gulp.watch(`${source}${assets}img/*.*`, ['images']);
+  gulp.watch(`${source}data/*.json`, ['nunjucks']);
+  gulp.watch(`${dest}index.html`).on('change', browserSync.reload);
 });
 
 // build task to populate assets
 gulp.task('build', ['clean'], () => {
-  gulp.start('nunjucks', 'fonts', 'styles', 'scripts', 'images');
+  gulp.start('nunjucks', 'fonts', 'styles', 'scripts', 'images', 'static');
 });
 
 // default task kicks off the build, runs BrowserSync, and watches for changes
